d35d95852ea8548991c150334af378339bf86bb1
alu0100851236 <alu0100851236@ull.edu.es>
2017-03-19 12:08:20 +0000

    creado el server

---
+++ src/server/app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var path = require('path')
+//var routes = require('.routes');
+//var user = require ('.routes/user');
+//var http = require('http);
+
+//instanciar
+var app = express();
+
+app.configure(function(){
+        app.set('views', __dirname + '/views');
+        app.set('view engine', 'jade');
+        app.use(express.favicon());
+        app.use(express.logger('dev'));
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(express.cookieParser('your secret here'));
+        app.use(express.session());
+        //app.use(app.router);
+        app.use(express.static(path.join(__dirname,'public')));
+}
+
+
+//ruteo
+app.get('/', routes.index)
+app.get('/users', user.list);
+
+app.get('/login', function(req, res){
+        res.render('login', title: 'login');
+)};
+
+//escuchar
+var server = app.listen(8180, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
