0bfbf6714311fba9bea7ed00826392607eca728f
alu0100851236 <alu0100851236@ull.edu.es>
2017-03-19 12:56:53 +0000

    arreglo server app.js

--- src/server/app.js
+++ src/server/app.js
@@ -1,36 +1,58 @@
-var express = require('express');
-var path = require('path')
-//var routes = require('.routes');
-//var user = require ('.routes/user');
-//var http = require('http);
+"use strict";
+let express = require('express'),
+    app = express(),
+    session = require('express-session');
+let cookieParser = require('cookie-parser');
+let path = require('path');
+let util = require("util");
+let router = express.Router();  //rutas
 
-//instanciar
-var app = express();
+//myserver_middleware.js
 
-app.configure(function(){
-        app.set('views', __dirname + '/views');
-        app.set('view engine', 'jade');
-        app.use(express.favicon());
-        app.use(express.logger('dev'));
-        app.use(express.bodyParser());
-        app.use(express.methodOverride());
-        app.use(express.cookieParser('your secret here'));
-        app.use(express.session());
+//module.exports = router;
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//        app.set('views', __dirname + '/views');
+//        app.set('view engine', 'jade');
+//        app.use(express.favicon());
+//        app.use(express.logger('dev'));
+//        app.use(express.bodyParser());
+//        app.use(express.methodOverride());
+//        app.use(express.cookieParser('your secret here'));
+//        app.use(express.session());
         //app.use(app.router);
-        app.use(express.static(path.join(__dirname,'public')));
-}
+//        app.use(express.static(path.join(__dirname,'public')));
 
 
 //ruteo
-app.get('/', routes.index)
-app.get('/users', user.list);
+//app.get('/', routes.index)
+//app.get('/users', user.list);
+
+app.get('/', function(req, res){
+//res.sendfile(__dirname + '/gh-pages/index.html');
+console.log('peticion a la /')
+});
 
 app.get('/login', function(req, res){
         res.render('login', {title: 'login'});
-)};
+});
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'Pruebas_Carlos_David',
+    resave: true,
+    saveUninitialized: false
+}))
+
+app.use(function(req, res, next) {
+  console.log("Cookies :  "+util.inspect(req.cookies));
+  console.log("session :  "+util.inspect(req.session));
+  next();
+});
 
 //escuchar
-var server = app.listen(8180, function () {
+var server = app.listen(8091, function () {
 
   var host = server.address().address
   var port = server.address().port
